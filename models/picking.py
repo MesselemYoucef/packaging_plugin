edc4470ca24dd7261e2f2f18bd4f2c291f5129a0
Youcef Messelem <messelemy@gmail.com>
2021-08-26 16:10:49 +0400

    Tried to fix the same error

--- models/picking.py
+++ models/picking.py
@@ -2,11 +2,7 @@ from odoo import models, fields, api
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.move'
-    # _inherit = ['stock.move', 'stock.move.line']
-
-    # product_id = fields.Many2one("product.template")
-    # pcs_per_cartoon = fields.Integer(related="product_id.pcs_per_cartoon", string="Number of cartons")
+    _inherit = 'stock.move.line'
     pcs_per_cartoon = fields.Integer(string="Number of cartons")
 
 
